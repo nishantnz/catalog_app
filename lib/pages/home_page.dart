46a311846e30acb7a29d46764a9f7d33a111c734
nishantnz <nishant.z@somaiya.edu>
2023-02-20 22:15:46 +0530

    ..

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,7 +9,9 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Catalog App"),
+        title: Text(
+          "Catalog App",
+        ),
       ),
       body: Material(
         child: Center(
