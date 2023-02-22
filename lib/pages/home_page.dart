47f2acb16546f656a2a1303ffd7c6817131de4b2
nishantnz <nishant.z@somaiya.edu>
2023-02-22 15:51:20 +0530

    function to load json data

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,16 +1,37 @@
 import 'package:catalog_app/models/catalog.dart';
 import 'package:catalog_app/widgets/itemWidget.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import '../widgets/drawer.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    print(productsData);
+  }
 
   @override
   Widget build(BuildContext context) {
     final dummyList =
-        List.generate(50, ((index) => CatalogModel.items[0]), growable: true);
+        List.generate(20, ((index) => CatalogModel.items[0]), growable: true);
     return Scaffold(
       appBar: AppBar(
         title: Text(
