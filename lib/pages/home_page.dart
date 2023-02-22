079cf173e310c49bdcbba8e5e7af7d947f21b343
nishantnz <nishant.z@somaiya.edu>
2023-02-22 14:44:39 +0530

    created a list generator and added listView

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:catalog_app/models/catalog.dart';
+import 'package:catalog_app/widgets/itemWidget.dart';
 import 'package:flutter/material.dart';
 
 import '../widgets/drawer.dart';
@@ -7,17 +9,23 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dummyList =
+        List.generate(50, ((index) => CatalogModel.items[0]), growable: true);
     return Scaffold(
       appBar: AppBar(
         title: Text(
           "Catalog App",
         ),
       ),
-      body: Material(
-        child: Center(
-          child: Container(
-            child: Text("hello"),
-          ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
         ),
       ),
       drawer: MyDrawer(),
